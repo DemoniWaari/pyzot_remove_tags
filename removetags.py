15bce720796db4b825af4886d05983932fe98aaf
Petri Mustonen <petri.mustonen@aalto.fi>
2022-02-03 16:15:06 +0200

    Moved everything to functions, did not test but should be ok

--- removetags.py
+++ removetags.py
@@ -5,49 +5,70 @@ import secrets # Api key and ID from secrets.py
 def chunks(l,n):
   for i in range(0, len(l), n):
     yield l[i:i+n]
-    
-# Make Zotero connection
-library_id = secrets.library_id
-api_key = secrets.api_key
-library_type = 'user'
-zot = zotero.Zotero(library_id, library_type, api_key)
 
-# Testing
-#all_tags = []
-#items = zot.top(limit=5)
-#for item in items:
-#  tags = item['data']['tags']
-#  all_tags.extend([v for dic in tags for k,v in dic.items()])
-#print(all_tags)
-#all_tags = set(all_tags) # unique values only
+# Make Zotero connection
+def make_connection(id, api):
+  library_type = 'user'
+  return zotero.Zotero(id, library_type, api)
 
 # Grab all tags and return all articles for a tag
 # That the search is not case-sensitive!
 # Add tags with only one article to-be-deleted list
-deleted_tags = []
-all_tags = zot.everything(zot.tags())
-for t in all_tags:
-  zot.add_parameters(tag=t) # Search by tag
-  tag_items = zot.items()
-  n_items = len(tag_items)
-  print(f'tag:{t} n:{n_items}')
-  if n_items == 1:
-    deleted_tags.append(t)
-    print(f'\t Assined for deletion:')
-# Write all tags to a file 
-print('Writing tags to file')
-with open('deletetags.txt','w') as f:
-  for t in deleted_tags:
-    f.write('%s\n' % t)
+def get_tags_and_filter(zot, taglist):
+  all_tags = zot.everything(zot.tags())
+  for t in all_tags:
+    zot.add_parameters(tag=t) # Search by tag
+    tag_items = zot.items()
+    n_items = len(tag_items)
+    print(f'tag:{t} n:{n_items}')
+    if n_items == 1:
+      taglist.append(t)
+      print(f'\t Assined for deletion:')
+  return taglist  
+
+def write_to_file(taglist, myfile):
+  # Write all tags to a file 
+  print('Writing tags to file')
+  with open(myfile,'w') as f:
+    for t in taglist:
+      f.write('%s\n' % t)
 
 # Delete tags
-print(f'Deleting tags: {deleted_tags}')
-# If more than 50 then we need to do a loopy loop
-if len(deleted_tags) > 50:
-  chunked = list(chunks(deleted_tags, 50)) # Split list
-  for l in chunked:
-    zot.delete_tags(*l)
-else:
-  zot.delete_tags(*deleted_tags)
-n_tags = len(deleted_tags)
-print(f'Deleted {n_tags} tags.')
+def del_tags(zot, taglist):
+  print(f'Deleting tags: {taglist}')
+  # If more than 50 then we need to do a loopy loop
+  if len(taglist) > 50:
+    chunked = list(chunks(taglist, 50)) # Split list
+    for l in chunked:
+      zot.delete_tags(*l)
+  else:
+    zot.delete_tags(*taglist)
+  n_tags = len(taglist)
+  print(f'Deleted {n_tags} tags.')
+  return 0
+
+def main():
+  # Do connection
+  zot = make_connection(secrets.library_id, secrets.api_key)
+  deleted_tags = [] # will be populated by tags with one article
+  # Get to be deleted tags
+  deleted_tags = get_tags_and_filter(zot, deleted_tags)
+  # Write them to file just to be sure
+  write_to_file(deleted_tags, 'deletetags.txt')
+  # Delete them
+  del_tags(zot, deleted_tags)
+  
+if __name__ == "__main__":
+  main()
+  
+  
+
+# Testing, returns tags of 5 newest articles
+#def test(zot)
+  #all_tags = []
+  #items = zot.top(limit=5)
+  #for item in items:
+  #  tags = item['data']['tags']
+  #  all_tags.extend([v for dic in tags for k,v in dic.items()])
+  #print(all_tags)
+  #return set(all_tags) # unique values only
